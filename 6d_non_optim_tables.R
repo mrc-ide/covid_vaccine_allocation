322666330631d32f3193320c1c8ca8b71c9cf62e
Alexandra Hogan <ahogan@dide.local>
2020-11-11 15:58:24 +0000

    updates to tidy code

--- 6d_non_optim_tables.R
+++ 6d_non_optim_tables.R
@@ -2,10 +2,10 @@ d1 <- read_csv("optim_dataframes/non_optim_allocation_income_cov1.csv") %>%
   mutate(income_group = factor(income_group, levels = c("LIC","LMIC", "UMIC", "HIC")))
 
 d2 <- read_csv("optim_dataframes/non_optim_allocation_income_cov2.csv") %>%
-  mutate(income_group = factor(income_group, levels = c("LIC","LMIC", "UMIC", "HIC")))
+  mutate(income_group = factor(income_group, levels = c("LIC","LMIC", "UMIC", "HIC"))) %>%
+  filter(sensitivity_run == "default") %>%
+  select(-sensitivity_run)
 
-# show allocation results
-write_csv(rbind(d1, d2), "tables/non_optim_results.csv")
 
 # create a summary table
 d_summ <- rbind(d1, d2) %>%
@@ -18,3 +18,28 @@ d_summ <- rbind(d1, d2) %>%
 
 # save summary table
 write_csv(d_summ, "tables/non_optim_results_summary_table_2_S2.csv")
+
+# sensitivity analysis table - table S4
+d_table <- read_csv("optim_dataframes/non_optim_allocation_income_cov2.csv") %>%
+  mutate(income_group = factor(income_group, levels = c("LIC","LMIC", "UMIC", "HIC"))) %>%
+  mutate(dap100d_2021 = round(dapd_2021 * 100, 3),
+         global_pop = 7646445824,
+         deaths_averted_per_m = round(deaths_averted_2021 / pop_2019 * 1e6),
+         total_deaths_averted_per_m = round(total_deaths_averted / global_pop * 1e6),
+         dap100d_total = round(total_deaths_averted / total_vaccine_n * 100, 3)) %>%
+  select(sensitivity_run, strategy, income_group, target_group, deaths_averted_per_m, dap100d_2021, total_deaths_averted_per_m, dap100d_total)
+
+# supplementary table - all details
+write_csv(d_table, "tables/non_optim_results_summary_sensitivity.csv")
+
+# table s4
+tab_s4 <- d_table %>%
+  select(strategy, sensitivity_run, total_deaths_averted_per_m, dap100d_total) %>%
+  unique() %>%
+  mutate(measure = paste0(total_deaths_averted_per_m, " (", dap100d_total, ")")) %>%
+  arrange(strategy) %>%
+  select(strategy, sensitivity_run, measure) %>%
+  pivot_wider(names_from = "sensitivity_run", id_cols = "strategy", values_from = "measure")
+
+write_csv(tab_s4, "tables/table_s4.csv")
+
